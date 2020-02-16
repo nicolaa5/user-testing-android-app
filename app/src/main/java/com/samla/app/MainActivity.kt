a448b0649c42dea63c5b8364fd9a510f69ba21ce
NiekBijman <niekbijman@gmail.com>
2020-02-16 17:08:55 +0100

    Update project package naming

--- app/src/main/java/com/samla/app/MainActivity.kt
+++ app/src/main/java/com/samla/app/MainActivity.kt
@@ -1,13 +1,11 @@
-package com.example.myapplication
+package com.samla.app
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
-import com.example.samla.Samla
-import com.example.samla.SamlaBuilder
-import com.example.samla.funnel.FunnelManager
+import com.samla.sdk.Samla
 
 import kotlinx.android.synthetic.main.activity_main.*
 
--- app/src/test/java/com/samla/app/ExampleUnitTest.kt
+++ app/src/test/java/com/samla/app/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.samla.app
 
 import org.junit.Test
 
